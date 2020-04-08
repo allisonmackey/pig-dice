75a7ecee0228a28e60ad189638f9a6f164810af9
Allison Mackey <a.mackey693@gmail.com>
2020-04-08 15:34:56 -0700

    debug sumScore method.

--- README.md
+++ README.md
@@ -10,14 +10,15 @@ A site to play a game. Practicing our use of jQuery & JavaScript.
 
 ## Specifications
 
-
 |   Behavior Description   |  Input Ex.   |        Output Ex.        |
 |------------------------------|--------------|--------------------------|
 | 1. Create an array that counts to 6 but outputs a random number between 1 & 6    |  User press button  |  A # 1 - 6   |
 | 2. If the roll is > 1 the player can continue to roll  | > 1  |  Roll again  or hold? |
 | 3. If the roll is === 1  |   1   |   Next players turn   |
-| 4. Hold button ==> switches to another player | Press "hold" button | Next players turn. |
-| 5. Allows player to roll multiple times & sum numbers rolled | User presses roll button output > 1 |  User is prompted to roll again
+| 4. Hold button sums players total score & displays it | Press "hold" button | Sum of rolls is displayed to the user | 
+| 5. Hold button ==> switches to another player | Press "hold" button | Next players turn. |
+| 6. Allows player to roll multiple times & sum numbers rolled | User presses roll button output > 1 |  User is prompted to roll again 
+| 7. Determine who the winner 
 
 
 ## Setup/Installation Requirements
--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,98 @@
 //Business Logic 
-//for Player One -->
-function PlayerOne (){
+//for Player -->
+function Player () {
   this.playerName = "";
   this.totalScore = 0;
   this.turnScore = [];
-  this.holdButton = [];
 };
 
-//Global variable for random number generator: 
-var random = Math.floor(Math.random() * (+max - +min)) + +min;
-
-PlayerOne.prototype.rollDice = function() {
-  var min = 1;
-  var max = 6;
-
+Player.prototype.rollDice = function() {
   // var num = random; 
   // var num1 = turnScore;
   // var total = num + num1; 
-  
+  var random = Math.floor(Math.random() * 6) + 1;
   if (random === 1) {
-    turnScore = [];  // might need to be refactored once we have multi-player option
+    this.turnScore = [];  // might need to be refactored once we have multi-player option
   } else if (random !== 1) {
-    turnScore.push(random);
-  
+    this.turnScore.push(random);
+  }
   // if (total >= 3)  // goal is to only run this if the player hits "hold button"
   //   totalScore = total;
   // }
   // else {
   //   random.sumScore();
   // }
-  console.log("turn score", turnScore);
+  console.log("turn score", this.turnScore);
   console.log("Random", random);
 };
 
-PlayerOne.prototype.sumScore = function(){
-  var num = random; 
-  var num1 = turnScore;
-  var total = num + num1; 
-  totalScore.push(total);
-  console.log("total score", totalScore)
-};
+// "Hold Button" Method --> Adds to the total score
+Player.prototype.sumScore = function(){
+  for (var i = 0; i <= this.turnScore; i ++) {
+    var num = this.turnScore[i];
+    var total = num + this.totalScore
+    this.totalScore = total; 
+  }
+  console.log("total score", this.totalScore);
+  return 
+
+  // this.turnScore.forEach(function(num){
+  //   this.totalScore = this.totalScore + num;
+  // });
+   
+}
+//   var random = Math.floor(Math.random() * 5) + 1;
+//   var num = random; 
+//   var num1 = parseInt(this.turnScore);
+//   var total = num1; 
+//   this.totalScore = total;  
+//   console.log("total score", this.totalScore)
+// };
+
+// Optional "Game" Construct to keep track of which players turn it is, 
 
 // for Score --->
 // function Score () {
 //   this.currentScore = rollOne + rollTwo; 
 // }
 // Score.prototype.addScore = function() {
-// }
+//
 
-var playerOne = new PlayerOne(); 
-var turnScore = [];
-playerOne.rollDice();
+// var playerOne = new Player(); 
+// var playerTwo = new Player();
+// var turnScore = [];
+// playerOne.rollDice();
 // playerOne.sumScore();
 
 
-// //User Interface Logic -----------------
-// $(document).ready(function() {
-//   $("form").submit(function(event) {
-//     event.preventDefault();
-//     rollDice = function() {
-//       var min = 1;
-//       var max = 6;
-//       var random = Math.floor(Math.random() * (+max - +min)) + +min;
-//       this.rollOne.push(random);
-//       console.log("roll one", rollOne);
-//       console.log("Dice", Dice);
-//       console.log("Random", random);
-//   };
-//   });
-// });
+//User Interface Logic ----------------->
+//creating function for hold button that calls on the .sumScore method when it's clicked 
+// testing add function of .sumScore method when said button is clicked
+// .append() | .html()
+// if we set turnscore to 0 var total would = random number rolled + 0, and on next roll it would be random number rolled + previously stored integer in turnscore.
+
+$(document).ready(function() {
+  var playerOne = new Player(); 
+  $("button#roll").click(function() {
+    var output = playerOne.rollDice();
+  $("button#hold").click(function(){  
+    return playerOne.sumScore();
+  });
+    console.log("output", output);
+    //   playerOne.rollDice();
+    // });
+    
+   
+   
+    
+    $("#output").append(output);
+  });
+
+  
+});
+
+
+
+//  $('#MyButton').click(function () {
+//       rollDice();
+//     });
